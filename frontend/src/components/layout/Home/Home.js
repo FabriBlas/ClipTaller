080029e720cf4d6098dcd17242e2d9974d740d91
Fabricio Blas <fabricio.blas@tallertechnologies.com>
2020-11-27 17:26:14 -0300

    proyecto

---
+++ frontend/src/components/layout/Home/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navigation from '../../common/Navigation'
+
+import Scroll from '../../../img/scroll_down_icon.svg'
+
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="bg-home">
+
+            <div className="header-content">
+                <Navigation />
+                <div className="container-title">
+                    <div>
+                    <h1 className="title">SKYE Texas</h1>
+                    <p className="title">Hill Country Resort</p>
+                    </div>
+                    <div className="text-header">
+                        <p>Experience nature in remarkable confort at this luxurious new concept in outdoor hospitality.</p>
+                        <p>The Texas Hill Country region is an exciting vacation destination featuring Fredericksburg, the quaint historic German town, and surrounded by the Texas Wine Country.</p>
+                    </div>
+                    <div className="scroll-down">
+                        <p>SCROLL DOWN</p>
+                        <img src={Scroll} alt="Scroll Down icon"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
