ccd060d1bedb978be68892b6555bddc24cc06c13
Fabricio Alan Blas <fabricioalanblas@gmail.com>
2021-01-18 15:27:57 -0300

    proyecto taller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,8 @@
 const app = require('./app');
 
 function main(){
-    const port = process.env.PORT || 5000;
-    
-    app.listen(port, () => console.log(`Server running on port ${port}`));
+  const port = process.env.PORT || 5000;  
+  app.listen(port, () => console.log(`Server running on port ${port}`));
 }
 
 main();
